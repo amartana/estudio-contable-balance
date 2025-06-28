9ff12ed0df074069c0caa514e76cb2879f2b3c01
amartana <gisela12650@gmail.com>
2025-06-28 03:03:19 -0300

    cuarto avance

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
-const mainMenu  = document.getElementById('navbar__menu-icon')
-const mainClose  = document.getElementById('navbar__menu-close')
-const mainNav  = document.getElementById('main-nav')
-const moreServices  = document.getElementById('services__types-title')
-const allServices  = document.getElementById('services__types')
+const mainMenu = document.getElementById('navbar__menu-icon')
+const mainClose = document.getElementById('navbar__menu-close')
+const mainNav = document.getElementById('main-nav')
+const moreServices = document.getElementById('services__types-title')
+const allServices = document.getElementById('services__types')
 const closeMoreServices = document.getElementById('services__types-close')
 const moreServicesFlecha = document.getElementById('services__types-title-flecha')
+const linkWa = document.getElementById('link-whatsapp')
+const msjsWa = {
+    basic: '¡Hola! Quisiera más información sobre sus servicios.', 
+    nuevo: '¡Hola!​ Quiero asesorarme para darme de alta como nuevo contribuyente.'
+}
 
+
+
+const linkWaBasic = () => {
+    let link = `https://wa.me/5491173636615?text=${encodeURIComponent(msjsWa.basic)}`
+    linkWa.href = link
+    console.log(link)
+}
 
 
 const toggleMenu = () => {
@@ -14,17 +26,15 @@ const toggleMenu = () => {
 }
 
 const toggleAllServices = () => {
-    if(allServices.classList.contains('inactive')) {
-       allServices.classList.toggle('inactive')
-    moreServicesFlecha.style.transform = 'rotateX(180deg) translate(0px, -2px)' 
-    allServices.style.boxShadow = ('2px 2px 2px 2px #f5efef')
+    if (allServices.classList.contains('inactive')) {
+        allServices.classList.toggle('inactive')
+        moreServicesFlecha.style.transform = 'rotateX(180deg) translate(0px, -2px)'
+        allServices.style.boxShadow = ('2px 2px 2px 2px #f5efef')
     } else {
         allServices.classList.toggle('inactive')
-moreServicesFlecha.style.transform = 'rotate(0deg)'
-    allServices.style.boxShadow = ('none')
+        moreServicesFlecha.style.transform = 'rotate(0deg)'
+        allServices.style.boxShadow = ('none')
     }
-     
-    
 }
 
 
@@ -32,3 +42,4 @@ mainMenu.addEventListener('click', toggleMenu)
 mainClose.addEventListener('click', toggleMenu)
 moreServices.addEventListener('click', toggleAllServices)
 closeMoreServices.addEventListener('click', toggleAllServices)
+linkWa.addEventListener('click', linkWaBasic)
