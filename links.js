78fc834e1077c2a5b0a18e1d2e84edf6ac25ec60
amartana <gisela12650@gmail.com>
2025-06-28 21:14:02 -0300

    quinto avance

---
+++ links.js
@@ -0,0 +1,19 @@
+const imagenes = {
+    portada: 'https://iili.io/FRtbyes.jpg',
+    logo: 'https://iili.io/FRtdQe9.png',
+    iconosServicios: {
+        deudas: 'https://iili.io/FRtdsJS.png',
+        online: 'https://iili.io/FRtdbWb.png',
+        nuevos: 'https://iili.io/FRtdDzu.png',
+        mensuales: 'https://iili.io/FRtdZbe.png',
+        internet: '',
+        servicioExpress: 'https://iili.io/FRtdPg2.png'
+    },
+    iconosPagina: {
+        logoDocument: 'https://iili.io/FRtdL57.png',
+        wa: 'https://iili.io/FRt2FmF.md.png',
+    },
+    avatar: 'https://iili.io/FRDeJgS.png',
+    fondoContacto: 'https://iili.io/FRtbp7n.md.jpg'
+
+}
